98ce8c133f889596d67e8d2c91e164777c0c45db
Anthony-Xu <hxu1314@outlook.com>
2017-06-12 21:20:12 +0800

    Evaluation Function Version 2

--- AI5.py
+++ AI5.py
@@ -31,7 +31,6 @@ def strategy(state):
                     table[i, j] = 1
                 elif (i+1, j+1) in board[1]:
                     table[i, j] = 2
-
     # 获取改点4个方向的棋型
     def getstring(point):
         x = point[0]
@@ -77,28 +76,20 @@ def strategy(state):
                 or '*11110' in getline or '01111*' in getline:
             return 'die4'
         if '11101' in getline or '10111' in getline\
-            or '11011' in getline :
+            or '11011' in getline:
             return 'lowdie4'
         if '001110' in getline or '011100' in getline:
             return 'alive3'
-        if '2011102' in getline or '211100' in getline \
+        if '211100' in getline or '001112' in getline\
+            or '*11100' in getline or '00111*' in getline\
+            or '211010' in getline or '010112' in getline\
+            or '*11010' in getline or '01011*' in getline\
+            or '210110' in getline or '011012' in getline\
+            or '*10110' in getline or '01101*' in getline\
+            or '11001' in getline or '10011' in getline\
+            or '10101' in getline or '2011102' in getline\
             or '*011102' in getline or '201110*' in getline\
-            or '*01110*' in getline or '*11100' in getline\
-            or '00111*' in getline or '011001' in getline\
-            or '100110' in getline \
-            or '001112' in getline or '11001' in getline\
-            or '10011' in getline or '011012' in getline\
-            or '210110' in getline or '*10110' in getline\
-            or '01101*' in getline\
-            or '21101' in getline or '*1101' in getline\
-            or '1011*' in getline\
-            or '10112' in getline or '10101' in getline\
-            or '*11001' in getline or '10011*' in getline\
-            or '211001' in getline or '100112' in getline\
-            or '1011*' in getline or '*1101' in getline\
-            or '10112' in getline or '21101' in getline\
-            or '10011*' in getline or '*11001' in getline\
-            or '100112' in getline or '211001' in getline:
+            or '*01110*' in getline:
             return 'die3'
         if '011010' in getline or '010110' in getline:
             return 'tiao3'
@@ -108,7 +99,12 @@ def strategy(state):
             or '010010' in getline:
             return 'lowalive2'
         if '211000' in getline or '000112' in getline\
-                or '*11000' in getline or '00011*' in getline:
+                or '*11000' in getline or '00011*' in getline\
+                or '210100' in getline or '001012' in getline\
+                or '*10100' in getline or '00101*' in getline\
+                or '210010' in getline or '010012' in getline\
+                or '*10010' in getline or '01001*' in getline\
+                or '10001' in getline:
             return 'die2'
         else:
             return 'nothreat'
@@ -127,21 +123,16 @@ def strategy(state):
             return 'lowdie4'
         if '002220' in getline or '022200' in getline:
             return 'alive3'
-        if '1022201' in getline or '122200' in getline \
-            or '*02220*' in getline or '102220*' in getline or '*022201' in getline\
+        if '122200' in getline or '002221' in getline\
             or '*22200' in getline or '00222*' in getline\
-            or '002221' in getline or '22002' in getline\
-            or '20022' in getline or '022021' in getline\
-            or '02202*' in getline or '*20220' in getline\
-            or '120220' in getline or '12202' in getline\
-            or '*2202' in getline or '2022*' in getline\
-            or '20221' in getline or '20202' in getline\
-            or '*22002' in getline or '20022*' in getline\
-            or '122002' in getline or '200221' in getline\
-            or '2022*' in getline or '*2202' in getline\
-            or '20221' in getline or '12202' in getline\
-            or '20022*' in getline or '*22002' in getline\
-            or '200221' in getline or '122002' in getline:
+            or '122020' in getline or '020221' in getline\
+            or '*22020' in getline or '02022*' in getline\
+            or '120220' in getline or '022021' in getline\
+            or '*20220' in getline or '02202*' in getline\
+            or '22002' in getline or '20022' in getline\
+            or '20202' in getline or '1022201' in getline\
+            or '*022201' in getline or '102220*' in getline\
+            or '*02220*' in getline:
             return 'die3'
         if '022020' in getline or '020220' in getline:
             return 'tiao3'
@@ -150,7 +141,13 @@ def strategy(state):
         if '002020' in getline or '020200' in getline\
             or '020020' in getline:
             return 'lowalive2'
-        if '22000' in getline or '00022' in getline:
+        if '122000' in getline or '000221' in getline\
+                or '*22000' in getline or '00022*' in getline\
+                or '120200' in getline or '002021' in getline\
+                or '*20200' in getline or '00202*' in getline\
+                or '120020' in getline or '020021' in getline\
+                or '*20020' in getline or '02002*' in getline\
+                or '20002' in getline:
             return 'die2'
         else:
             return 'nothreat'
@@ -163,54 +160,19 @@ def strategy(state):
             for j in range(col):
                 if table[i, j] == 1:
                     point = (i, j)
-                    myType={'win5':0, 'alive4':0, 'die4':0, 'lowdie4':0, 'alive3':0, 'die3':0, 'tiao3':0, 'alive2':0, 'lowalive2':0, 'die2':0, 'nothreat':0}
+                    myType={'win5':0, 'alive4':0, 'die4':0, 'lowdie4':0, 'alive3':0, 'die3':0, 'tiao3':0, 'alive2':0, \
+                            'lowalive2':0, 'die2':0, 'nothreat':0}
                     lines = getstring(point)
                     for item0 in lines:
                         tmp1 = judgeType1(item0)
                         myType[tmp1] += 1
                     # my score
-                    # 赢5
-                    if myType['win5']>=1:
-                        myscore+=100000
-                    # 活4 双死4 死4活3
-                    elif myType['alive4']>=1 or myType['die4']>=2 or (myType['die4']>=1 and myType['alive3']>=1):
-                        myscore+=10000
-                    # 双活3
-                    elif myType['alive3']>=2:
-                        myscore+=5000
-                    # 死3高级活3
-                    elif myType['die3']>=1 and myType['alive3']>=1:
-                        myscore+=1000
-                    # 高级死4
-                    elif myType['die4']>=1:
-                        myscore+=500
-                    # 低级死4
-                    elif myType['lowdie4']>=1:
-                        myscore+=400
-                    # 单活3
-                    elif myType['alive3']>=1:
-                        myscore+=100
-                    # 跳活3
-                    elif myType['tiao3']>=1:
-                        myscore+=90
-                    # 双活2
-                    elif myType['alive2']>=2:
-                        myscore+=50
-                    # 活2
-                    elif myType['alive2']>=1:
-                        myscore+=10
-                    # 低级活2
-                    elif myType['lowalive2']>=1:
-                        myscore+=9
-                    # 死3
-                    elif myType['die3']>=1:
-                        myscore+=5
-                    # 死2
-                    elif myType['die2']>=1:
-                        myscore+=2
-                    # 没有威胁
-                    else:
-                        myscore+=1
+                    myscore += 1000000*myType['win5']+300000*myType['alive4']+ \
+                               2500 * myType['die4']+3000*myType['lowdie4']+ \
+                               3000 * myType['alive3']+800*myType['tiao3']+ \
+                               600 * myType['die3']+650*myType['alive2']+ \
+                               250 * myType['lowalive2']+200*myType['lowalive2']+ \
+                               5 * myType['nothreat']
         return myscore
 
     # 计算敌方的形势分数
@@ -221,71 +183,37 @@ def strategy(state):
             for j in range(col):
                 if table[i, j] == 2:
                     point = (i, j)
-                    opType = {'win5': 0, 'alive4': 0, 'die4': 0, 'lowdie4': 0, 'alive3': 0, 'die3': 0, 'tiao3': 0, 'alive2': 0,
-                              'lowalive2': 0, 'die2': 0, 'nothreat': 0}
+                    opType = {'win5': 0, 'alive4': 0, 'die4': 0, 'lowdie4': 0, 'alive3': 0, 'die3': 0, 'tiao3': 0, \
+                              'alive2': 0, 'lowalive2': 0, 'die2': 0, 'nothreat': 0}
                     lines = getstring(point)
                     for item0 in lines:
                         tmp2 = judgeType2(item0)
                         opType[tmp2] += 1
                     # opponent score
                     # 赢5
-                    if opType['win5'] >= 1:
-                        opscore += 100000
-                    # 活4 双死4 死4活3
-                    elif opType['alive4'] >= 1 or opType['die4'] >= 2 or (opType['die4'] >= 1 and opType['alive3'] >= 1):
-                        opscore += 10000
-                    # 双活3
-                    elif opType['alive3'] >= 2:
-                        opscore += 5000
-                    # 死3高级活3
-                    elif opType['die3'] >= 1 and opType['alive3'] >= 1:
-                        opscore += 1000
-                    # 高级死4
-                    elif opType['die4'] >= 1:
-                        opscore += 500
-                    # 低级死4
-                    elif opType['lowdie4'] >= 1:
-                        opscore += 400
-                    # 单活3
-                    elif opType['alive3'] >= 1:
-                        opscore += 100
-                    # 跳活3
-                    elif opType['tiao3'] >= 1:
-                        opscore += 90
-                    # 双活2
-                    elif opType['alive2'] >= 2:
-                        opscore += 50
-                    # 活2
-                    elif opType['alive2'] >= 1:
-                        opscore += 10
-                    # 低级活2
-                    elif opType['lowalive2'] >= 1:
-                        opscore += 9
-                    # 死3
-                    elif opType['die3'] >= 1:
-                        opscore += 5
-                    # 死2
-                    elif opType['die2'] >= 1:
-                        opscore += 2
-                    # 没有威胁
-                    else:
-                        opscore += 1
+                    # my score
+                    opscore += 1000000 * opType['win5'] + 300000 * opType['alive4'] + \
+                               2500 * opType['die4'] + 3000 * opType['lowdie4'] + \
+                               3000 * opType['alive3'] + 800 * opType['tiao3'] + \
+                               600 * opType['die3'] + 650 * opType['alive2'] + \
+                               250 * opType['lowalive2'] + 200 * opType['lowalive2'] + \
+                               5 * opType['nothreat']
         return opscore
 
-    # 下棋思路：当前位置为空，计算我方形势分数-敌方形式分数，然后将此位置令成1，再次计算我方形势分数-敌方形式分数。两次相减，即为下该点的分数。遍历所有空点
     scoretable={}
+    defend = 2
     for i in range(row):
         for j in range(col):
             if table[i, j] == 0:
-                old = evaluate_self(table)-evaluate_op(table)
+                #old = evaluate_self(table)-defend*evaluate_op(table)
                 table[i, j] = 1
-                new = evaluate_self(table)-evaluate_op(table)
-                scoretable[(i, j)] = new-old
+                scoretable[(i, j)] = evaluate_self(table)-defend*evaluate_op(table)
                 table[i, j] = 0
     self_position = max(scoretable.items(), key=lambda x: x[1])[0]
     return (self_position[0]+1, self_position[1]+1)
 
-    # 另一种下棋思路：找出我方形势分数的最大值mymaxscore及其对应的位置，找出敌方形势的最大值hismaxscore及其对应的位置。如果mymaxscore>=hismaxscore，则进攻，下我方形势最大值mymaxscore对应的位置
+    # 另一种下棋思路：找出我方形势分数的最大值mymaxscore及其对应的位置，找出敌方形势的最大值hismaxscore及其对应的位置。
+    # 如果mymaxscore>=hismaxscore，则进攻，下我方形势最大值mymaxscore对应的位置
     # 否则，防守，下敌方形势最大值hismaxscore对应的位置。
     '''
     for i in range(row):
